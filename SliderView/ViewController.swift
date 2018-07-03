36d7893ae1ee054b12ade8cdd729d9eebdd3f5ce
zzq <1521154719@qq.com>
2018-07-03 16:53:56 +0800

    no message

--- SliderView/ViewController.swift
+++ SliderView/ViewController.swift
@@ -11,14 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     var mainView: UICollectionView!
-    
+    var sliderView: ZQSliderView!
     let titles = ["推荐", "娱乐", "生活", "游戏", "汽车", "纪录片", "拍客", "路由", "科技", "财经", "原创"]
-    var viewArray = [UITableView]()
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         
         self.creatSubview()
     }
@@ -30,27 +29,91 @@ class ViewController: UIViewController {
 
     //MARK:
     func creatSubview() {
+       
         
-        mainView = UIScrollView.init(frame: CGRect(x: 0, y: 64, width: UIView.screenWidth(), height: UIView.screenHeight()-64))
+        sliderView = ZQSliderView(frame: CGRect(x: 0, y: 20, width: UIView.screenWidth(), height: 44), titles: titles)
+        self.view.addSubview(sliderView)
+        
+        
+        let itemWidth = UIView.screenWidth()
+        let itemHeight = UIView.screenHeight()-sliderView.bottom
+
+        let layout = UICollectionViewFlowLayout.init()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        mainView = UICollectionView(frame: CGRect(x: 0, y: sliderView.bottom, width: itemWidth, height: itemHeight), collectionViewLayout: layout)
         self.view.addSubview(mainView)
+        mainView.isPagingEnabled = true
+        mainView.showsHorizontalScrollIndicator = false
+        mainView.backgroundColor = UIColor.white
+        mainView.dataSource = self
+        mainView.delegate = self
+        mainView.bounces = false
+        mainView.register(ZQCell.classForCoder(), forCellWithReuseIdentifier: "zqcell")
+
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "zqcell", for: indexPath) as? ZQCell
+        cell?.tv.text = titles[indexPath.item]
+        return cell!
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        var dir = 1
+        let x = scrollView.contentOffset.x
         
-        let itemWidth = mainView.width
-        let itemHeight = mainView.height
-        
-        
-        for (index, _) in titles.enumerated() {
-            let tab = UITableView(frame: CGRect(x: CGFloat(index)*itemWidth, y: 0, width: itemWidth, height: itemHeight), style: .plain)
-            mainView.addSubview(tab)
-            tab.separatorStyle = .none
-            tab.addSubview(TempleteView(frame: tab.bounds))
+        if x<CGFloat(self.sliderView.currentIndex.item)*UIView.screenWidth() {
+            dir = -1
         }
-        mainView.contentSize = CGSize(width: CGFloat(titles.count)*itemWidth, height: itemHeight)
+        
+        self.sliderView.moveLine(offsetX: x, stan: UIView.screenWidth()/2, direction: dir)
+    }
+    
+}
+
+
+class ZQCell: UICollectionViewCell {
+    
+    var tv: TempleteView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        tv = TempleteView(frame: self.bounds)
+        self.contentView.addSubview(tv)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
 class TempleteView: UIView {
+    var text: String? {
+        didSet {
+            if let a = self.viewWithTag(10) {
+                a.removeFromSuperview()
+                
+            }
+            
+            let la = UILabel.creatLabel(frame: CGRect(x: 10, y: 10, width: self.width-20, height: 30), title: text, titleColor: UIColor.red, font: UIFont.systemFont(ofSize: 15), textAlignment: .center)
+            la.tag = 10
+            self.addSubview(la)
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
- 
+
         let layer1 = CALayer.init()
         self.layer.addSublayer(layer1)
         layer1.backgroundColor = UIColor.rgba(red: 230, green: 230, blue: 230, alpha: 1).cgColor
@@ -66,6 +129,8 @@ class TempleteView: UIView {
         self.layer.addSublayer(layer3)
         layer3.backgroundColor = UIColor.rgba(red: 230, green: 230, blue: 230, alpha: 1).cgColor
         layer3.frame = CGRect(x: 10, y: 170, width: self.width-20, height: 20)
+        
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
--- SliderView/ZQSliderView.swift
+++ SliderView/ZQSliderView.swift
@@ -8,8 +8,189 @@
 
 import UIKit
 
+typealias kCompletion = () -> Void
+
+//enum AnimationKey {
+//    case gorup
+//}
+
 class ZQSliderView: UIView {
 
+    var titleView: UICollectionView!
+    var line: UIView!
+    var titles: [String]!
+    var lineColor =  UIColor.red
     
+    var currentIndex = IndexPath.init(item: 0, section: 0)
+    var preIndex: IndexPath?
+    
+    let duration = 0.3
+    let itemWidth: CGFloat = 65
+    let key = "animationKey"
+    
+    
+    var animationArray = [kCompletion]()
+    
+    init(frame: CGRect, titles: [String]) {
+        super.init(frame: frame)
+        self.titles = titles
+        self.creatSubview()
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK:
+    func creatSubview() {
+        
+        let itemHeight: CGFloat = self.height-1
+        
+        let layout = UICollectionViewFlowLayout.init()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        titleView = UICollectionView(frame: CGRect(x: 0, y: 0, width: self.width, height: itemHeight), collectionViewLayout: layout)
+        self.addSubview(titleView)
+        
+        titleView.showsHorizontalScrollIndicator = false
+        titleView.backgroundColor = UIColor.white
+        titleView.dataSource = self
+        titleView.delegate = self
+        titleView.register(SliderCell.classForCoder(), forCellWithReuseIdentifier: "sdcell")
+        
+        line = UIView(frame: CGRect(x: 0, y: titleView.bottom-1, width: 20, height: 1))
+        titleView.addSubview(line)
+        line.backgroundColor = lineColor
+    }
+    
+    func moveLine(toIndex: IndexPath, completion: @escaping kCompletion) {
+        
+        line.layer.removeAllAnimations()
+        
+        let cell1 = titleView.cellForItem(at: currentIndex) as? SliderCell
+        let cell2 = titleView.cellForItem(at: toIndex) as? SliderCell
+        
+        
+        let animation1 = CAKeyframeAnimation(keyPath: "bounds")
+        animation1.keyTimes = [0, 0.5, 1]
+        animation1.values = [CGRect(x: 0, y: 0, width: 20, height: 1), CGRect(x: 0, y: 0, width: itemWidth, height: 1), CGRect(x: 0, y: 0, width: 20, height: 1)]
+        animation1.timingFunctions = [CAMediaTimingFunction.init(name: "easeOut"),CAMediaTimingFunction.init(name: "easeIn")]
+        animation1.repeatCount = 1
+        animation1.duration = duration
+        
+        let animation2 = CABasicAnimation.init(keyPath: "position")
+        animation2.fromValue = CGPoint(x: cell1!.centerX, y: line.centerY)
+        animation2.toValue = CGPoint(x: cell2!.centerX, y: line.centerY)
+        animation2.duration = duration
+        animation2.repeatCount = 1
+        
+        let group = CAAnimationGroup.init()
+        group.animations = [animation1, animation2]
+        group.duration = duration
+        group.repeatCount = 1
+        group.fillMode = kCAFillModeForwards
+        group.isRemovedOnCompletion = false
+        group.delegate = self
+        objc_setAssociatedObject(group, UnsafeRawPointer.init(bitPattern: key.hashValue)!, completion, .OBJC_ASSOCIATION_COPY_NONATOMIC)
+        debugPrint("\(group)")
+        line.layer.add(group, forKey: "gg")
+        
+        animationArray.append(completion)
+        
+        
+        cell2!.textLabel.textColor = UIColor.red
+        cell1!.textLabel.textColor = UIColor.rgba(red: 51, green: 51, blue: 51, alpha: 1)
+        self.preIndex = self.currentIndex
+        self.currentIndex = toIndex
+//        titleView.scrollToItem(at: toIndex, at: .centeredHorizontally, animated: true)
+    }
+    func moveLine(offsetX: CGFloat, stan: CGFloat, direction: Int)  {//left:-1, right:1
+        let cell1 = titleView.cellForItem(at: currentIndex) as? SliderCell
+        let cell2 = titleView.cellForItem(at: IndexPath(item: currentIndex.item+direction, section: 0)) as? SliderCell
+        if offsetX<stan {
+            line.width = 20+offsetX/stan*45
+            line.left = cell1!.centerX+offsetX/stan*5
+        }else {
+            line.width = 65-(offsetX-stan)/stan*45
+            line.centerX = cell2!.centerX+offsetX/stan*5
+        }
+    }
+}
+extension ZQSliderView : CAAnimationDelegate {
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+         debugPrint("\(anim)")
+//        if let com = objc_getAssociatedObject(anim, UnsafeRawPointer.init(bitPattern: key.hashValue)!)as?kCompletion  {
+//            com()
+//        }
+        print("\(Thread.current)")
+        if animationArray.count>0 {
+            animationArray[0]()
+        }
+        
+    }
+    func animationDidStart(_ anim: CAAnimation) {
+       
+    }
+}
+extension ZQSliderView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sdcell", for: indexPath) as? SliderCell
+        cell?.textLabel.text = titles[indexPath.item]
+        
+        if currentIndex.item == indexPath.item {
+            cell?.textLabel.textColor = UIColor.red
+            line.centerX = cell!.centerX
+        }else {
+            cell?.textLabel.textColor = UIColor.rgba(red: 51, green: 51, blue: 51, alpha: 1)
+        }
+        
+        return cell!
+    }
+}
 
+extension ZQSliderView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        guard indexPath.item != self.currentIndex.item else {
+            print("same")
+            return
+        }
+       
+        self.moveLine(toIndex: indexPath) {
+            print("completion")
+           collectionView.scrollToItem(at: self.currentIndex, at: .centeredHorizontally, animated: true)
+        }
+        
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+    }
+}
+
+class SliderCell: UICollectionViewCell {
+    var textLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        textLabel = UILabel(frame: self.bounds)
+        self.addSubview(textLabel)
+        
+        textLabel.textColor = UIColor.rgba(red: 51, green: 51, blue: 51, alpha: 1)
+        textLabel.font = UIFont.systemFont(ofSize: 15)
+        textLabel.textAlignment = .center
+        textLabel.backgroundColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
